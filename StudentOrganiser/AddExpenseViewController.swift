3c1a55a06beb54c2ae88c1643bb0284df6fc45f0
jacobbrennan <37852818+jacobbrennan@users.noreply.github.com>
2018-05-20 15:35:23 +1000

    Added View Controllers
    View Controllers added for expenses, add expenses and the table view

---
+++ StudentOrganiser/AddExpenseViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddExpenseViewController.swift
+//  StudentOrganiser
+//
+//  Created by Nick Haeusler on 20/5/18.
+//  Copyright © 2018 Jacob Brennan. All rights reserved.
+//
+
+import UIKit
+
+class AddExpenseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
