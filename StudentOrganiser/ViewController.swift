53f20a373ac4caebc2d9c83b2cc56d3fa78bd5f5
AnkushYamarti <Ankush.Yamarti@student.uts.edu.au>
2018-05-21 12:53:00 +1000

    testing github

--- StudentOrganiser/ViewController.swift
+++ StudentOrganiser/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    //test comment
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
