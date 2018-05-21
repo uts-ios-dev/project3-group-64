5f28c5e18da6824e70f252eb6fbffd9acf368de2
AnkushYamarti <Ankush.Yamarti@student.uts.edu.au>
2018-05-21 13:28:32 +1000

    Added View controller files for Notes.

---
+++ StudentOrganiser/NotesTableViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NotesTableViewController.swift
+//  StudentOrganiser
+//
+//  Created by Ankush Yamarti on 21/5/18.
+//  Copyright © 2018 Jacob Brennan. All rights reserved.
+//
+
+import UIKit
+
+class NotesTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+   
+    }
+
+
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+     
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       
+        return 0
+    }
+
+}
