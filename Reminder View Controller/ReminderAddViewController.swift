81b4df39fe36eda2f5a66d5fb6ff9a02da06bb75
Marcus <marcus.e.hugh@student.uts.edu.au>
2018-05-23 23:18:50 +1000

    Base functionality to Reminder
    Added base functionality to reminder, still has bugs, but commit before class

---
+++ Reminder View Controller/ReminderAddViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ReminderAddViewController.swift
+//  StudentOrganiser
+//
+//  Created by Marcus Hugh on 23/5/18.
+//  Copyright © 2018 Jacob Brennan. All rights reserved.
+//
+
+import UIKit
+
+class ReminderAddViewController: UIViewController {
+
+    @IBOutlet weak var reminderTitle: UITextField!
+    @IBOutlet weak var reminderBody: UITextView!
+    @IBOutlet weak var reminderSwitch: UISwitch!
+    @IBOutlet weak var reminderDate: UIDatePicker!
+    
+    var reminderTitles:[String] = []
+    var reminderBodys:[String] = []
+    var reminderDates = NSDate()
+    
+    @IBAction func dateSwitchOn(_ sender: UISwitch) {
+        if reminderSwitch.isOn {
+            reminderDate.alpha = 1
+        } else {
+            reminderDate.alpha = 0
+        }
+    }
+
+    
+    let userDefaults = Foundation.UserDefaults.standard
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if userDefaults.stringArray(forKey: "reminderTitles") != nil {
+            reminderTitles = userDefaults.stringArray(forKey: "reminderTitles")!
+        }
+        
+        if userDefaults.stringArray(forKey: "reminderBodys") != nil {
+            reminderBodys = userDefaults.stringArray(forKey: "reminderBodys")!
+        }
+
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+    @IBAction func AddReminder(_ sender: UIButton) {
+        
+        let title = reminderTitle.text!
+        let body = reminderBody.text!
+        
+        reminderTitles.append(title)
+        reminderBodys.append(body)
+        
+        userDefaults.set(reminderTitles, forKey: "reminderTitles")
+        userDefaults.set(reminderBodys, forKey: "reminderBodys")
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+
+    @IBAction func BackButton(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+
+    }
+
+}
---
+++ Reminder View Controller/ReminderTableViewController.swift
@@ -0,0 +1,78 @@
+//
+//  ReminderTableViewController.swift
+//  StudentOrganiser
+//
+//  Created by Marcus Hugh on 23/5/18.
+//  Copyright © 2018 Jacob Brennan. All rights reserved.
+//
+
+import UIKit
+
+class ReminderTableViewController: UITableViewController {
+
+    //My variables
+    var reminderTitles:[String] = []
+    let userDefaults = Foundation.UserDefaults.standard
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        getTitles()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        getTitles()
+    }
+    
+    func getTitles() {
+        if userDefaults.stringArray(forKey: "reminderTitles") != nil {
+            reminderTitles = userDefaults.stringArray(forKey: "reminderTitles")!
+            self.tableView.reloadData()
+            self.refreshControl?.endRefreshing()
+        }
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+            return reminderTitles.count
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reminderTitleCell", for: indexPath)
+        
+        let title = reminderTitles[indexPath.section]
+        
+        
+        cell.textLabel?.text = title
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let index = indexPath.section
+        userDefaults.set(index, forKey: "index")
+        
+        
+        let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let newViewController = storyBoard.instantiateViewController(withIdentifier: "newViewController")
+        
+        self.present(newViewController, animated: true, completion: nil)
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return ""
+    }
+
+}
